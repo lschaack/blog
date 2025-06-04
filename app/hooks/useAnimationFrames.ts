8356985fa3543f0672b41b6e9386bcb8bef0b07d
Luke Schaack <luke.schaack@gmail.com>
2025-06-04 11:41:48 -0700

    fix rare bug in frame rate checker

--- app/hooks/useAnimationFrames.ts
+++ app/hooks/useAnimationFrames.ts
@@ -17,7 +17,10 @@ export const useAnimationFrames = (callback: (delta: number) => void, enable = t
       const animate: FrameRequestCallback = (currTime: number) => {
         const delta = prevTime ? currTime - prevTime : 16.67; // assume 60fps
         const momentaryFps = 1000 / delta;
-        fps.current = lerp(fps.current, momentaryFps, 0.05);
+
+        // avoid getting stuck at Infinity when currTime === prevTime,
+        // not sure why it happens but very rare
+        if (momentaryFps < Infinity) fps.current = lerp(fps.current, momentaryFps, 0.05);
 
         callback(delta);
 
