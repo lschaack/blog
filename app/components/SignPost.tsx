f6570ac46cad462e72b5b3e294349c489fab115c
Luke Schaack <luke.schaack@gmail.com>
2025-06-03 18:13:13 -0700

    fix SignPost hover bug

--- app/components/SignPost.tsx
+++ app/components/SignPost.tsx
@@ -5,22 +5,35 @@ import clsx from 'clsx';
 
 export const SignPost: FC<{ children?: ReactNode }> = ({ children }) => {
   const [animate, setAnimate] = useState(false);
+  const [enableHover, setEnableHover] = useState(true);
 
   return (
     <div
-      className={clsx(
-        "perspective-normal",
-        "rotate-x-[8deg] -rotate-y-20",
-        //"transition-transform",
-        //"hover:-rotate-y-30",
-        animate && "animate-twist",
-      )}
+      className="group"
       onClick={() => {
-        if (!animate) setAnimate(true);
+        if (!animate) {
+          setAnimate(true);
+          setTimeout(() => setEnableHover(false), 300);
+        }
       }}
-      onAnimationEnd={() => setAnimate(false)}
+      onMouseMove={() => {
+        // Handle bug where user clicks and keeps mouse over the surface, resulting in a
+        // discontinuous jump to the group-hover rotation transform at the end of the animation
+        if (!enableHover && !animate) setEnableHover(true);
+      }}
     >
-      {children}
+      <div
+        className={clsx(
+          "perspective-normal",
+          "rotate-x-[8deg] -rotate-y-20",
+          "transition-transform",
+          enableHover && "group-hover:-rotate-y-30",
+          animate && "animate-twist",
+        )}
+        onAnimationEnd={() => setAnimate(false)}
+      >
+        {children}
+      </div>
     </div>
   );
 }
