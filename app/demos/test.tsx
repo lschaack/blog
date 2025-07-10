39998060438d0582d00421aceb324e1448dd52ff
Luke Schaack <luke.schaack@gmail.com>
2025-07-09 18:25:09 -0700

    add basic demo visualizer for circle packer results

--- app/demos/test.tsx
+++ app/demos/test.tsx
@@ -1,6 +1,31 @@
+import { CirclePacker } from "@/app/utils/circlePacker";
+
+const WIDTH = 1024;
+const HEIGHT = 1024;
+
+const packer = new CirclePacker({
+  width: WIDTH,
+  height: HEIGHT,
+  minRadius: 16,
+  maxRadius: 64,
+});
+
+packer.pack();
+
 export default function Demo() {
   return (
-    <div>
+    <div style={{ width: WIDTH, height: HEIGHT, position: 'relative' }}>
+      {packer.placedCircles.map(({ x, y, r }, i) => (
+        <div
+          key={`circle-${i}`}
+          className="absolute rounded-full bg-deep-700"
+          style={{
+            top: y,
+            left: x,
+            padding: r,
+          }}
+        />
+      ))}
     </div>
   );
 }
